4e0974886ef00176d605dbd528201e03605e96a5
Marcos Paulo <marcosphip7@gmail.com>
2020-05-26 17:11:02 -0300

    alteração

--- com110_lista7/com110_lista7_ex2_2020011248.c
+++ com110_lista7/com110_lista7_ex2_2020011248.c
@@ -5,11 +5,12 @@ int main(void) {
     int matriz[8][8];
     int i, j, num;
 
-    printf("Preencha a matriz com numeros inteiros: ");
+    printf("Preencha a matriz com numeros inteiros.\n");
     for (i = 0; i < 8; i++)
     {
         for (j = 0; j < 8; j++)
         {
+            printf("Posicao [%d][%d]: ", i + 1, j + 1);
             scanf(" %d", &matriz[i][j]);
         }
     }
