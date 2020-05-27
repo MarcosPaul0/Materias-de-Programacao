266f54c13b062c2e523fd83a623f537853aa4d31
Marcos Paulo <marcosphip7@gmail.com>
2020-05-27 16:58:27 -0300

    código melhorado

--- com110_lista6/com110_lista6_ex2_2020011248.c
+++ com110_lista6/com110_lista6_ex2_2020011248.c
@@ -2,27 +2,28 @@
 #include <stdlib.h>
 
 int main(void) {
-    int gabarito[8], prova_alunos[10][8], notas_alunos[10];
-    int i, m = 0, num_aprovados, porcentagem, nota = 0;
+    int notas_alunos[10];
+    int i, m = 0, num_aprovados = 0, nota = 0, porcentagem;
+    char gabarito[8], prova_alunos[10][8];
 
     //ENTRADA DO GABARITO
-    printf("----------- GABARITO -----------\n\n");
-    printf("As opções são [1] [2] [3] e [4].");
+    printf("\n----------- GABARITO -----------\n\n");
+    printf("As opcoes sao [a] [b] [c] e [d].\n\n");
     for (i = 0; i < 8; i++)
     {
         printf("Digite a resposta da questao [%d]: ", i + 1);
-        scanf("%d", &gabarito[i]);
+        scanf(" %c", &gabarito[i]);
     }
 
     //ENTTRADA DAS RESPOSTAS DOS ALUNOS
-    printf("----------- PROVA -----------\n\n");
+    printf("\n\n--------- PROVA DOS ALUNOS ---------\n\n");
     for (i = 0; i < 10; i++)
     {
         printf("Aluno %d: \n", i + 1);
         for (m = 0; m < 8; m++)
         {
             printf("Resposta da questao %d: ", m + 1);
-            scanf("%d", &prova_alunos[i][m]);
+            scanf(" %c", &prova_alunos[i][m]);
         }
     }
 
@@ -41,9 +42,10 @@ int main(void) {
     }
 
     //MOSTRANDO AS NOTAS INDIVIDUAIS
+    printf("\n\n");
     for (i = 0; i < 10; i++)
     {
-        printf("O aluno [%d] obteve nota %d.", i + 1, notas_alunos[i]);
+        printf("O aluno [%d] obteve nota %d.\n", i + 1, notas_alunos[i]);
         if (notas_alunos[i] >= 6)
         {
             num_aprovados += 1;
@@ -51,8 +53,8 @@ int main(void) {
     }
 
     //MOSTRANDO A PORCENTAGEM DE APROVADOS
-    porcentagem = (num_aprovados / 10) * 100;
-    printf("A porcentagem de aprovados foi de %d%%.", porcentagem);
+    porcentagem = num_aprovados * 10;
+    printf("\nA porcentagem de aprovados foi de %d%%.\n", porcentagem);
     
     return 0;
 }
