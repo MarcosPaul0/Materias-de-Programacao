c594ddddc16606cd012ee18e2a87125343e20d1a
Marcos Paulo <marcosphip7@gmail.com>
2020-08-26 13:00:34 -0300

    Exercício 2 concluído.

--- COM111/com111_lista1/com111_lista1_ex02_2020011248.c
+++ COM111/com111_lista1/com111_lista1_ex02_2020011248.c
@@ -4,17 +4,17 @@
 #include <locale.h>
 
 typedef struct aluno {
-    int matricula;
+    unsigned int matricula;
     char nome[50];
-    int diaNas;
-    int mesNas;
-    int anoNas;
+    unsigned int diaNas;
+    unsigned int mesNas;
+    unsigned int anoNas;
     float nota;
 };
 
-void preencheVetor(struct aluno *dados, int n) {
+void preencheVetor(struct aluno *dados, int numAlunos) {
     printf("\n\nPREENCHA OS DADOS DOS ALUNOS:\n\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < numAlunos; i++) {
         printf("Matrícula do aluno: ");
         scanf(" %d", &dados[i].matricula);
         printf("Nome do aluno: ");
@@ -27,32 +27,66 @@ void preencheVetor(struct aluno *dados, int n) {
         scanf(" %d", &dados[i].anoNas);
         printf("Nota: ");
         scanf(" %f", &dados[i].nota);
+        printf("\n");
     }
-    printf("\n\n");
 }
 
-void imprimeVetor(struct aluno *dados, int n) {
-    printf("DADOS DOS ALUNOS REGISTRADOS:\n\n");
-    for (int i = 0; i < n; i++) {
-        printf("Matrícula - %i\n", dados[i].matricula);
-        printf("Nome - %s\n", dados[i].nome);
-        printf("Data de nascimento - %d/%d/%d\n", dados[i].diaNas, dados[i].mesNas, dados[i].anoNas);
-        printf("Nota - %f\n\n", dados[i].nota);
+void preencheRegistro(struct aluno *dados, struct aluno *registro, int numAlunos, int numAprovados) {
+    int i, j = 0;
+
+    for (i = 0; i < numAlunos; i++) {
+        while (j < numAprovados) {
+            if (dados[j].nota >= 6) {
+                registro[j].matricula = dados[i].matricula;
+                strncpy(registro[j].nome, dados[i].nome, 50);
+                registro[j].diaNas = dados[i].diaNas;
+                registro[j].mesNas = dados[i].mesNas;
+                registro[j].anoNas = dados[i].anoNas;
+                registro[j].nota = dados[i].nota;
+            }
+            break;
+        }
+        j++;
     }
 }
+
+
+void imprimeVetor(struct aluno *dados, int numAlunos) {
+    printf("DADOS DOS ALUNOS APROVADOS:\n\n");
+    for (int i = 0; i < numAlunos; i++) {
+        printf("Matrícula - %i.\n", dados[i].matricula);
+        printf("Nome - %s.\n", dados[i].nome);
+        printf("Data de nascimento - %d/%d/%d.\n", dados[i].diaNas, dados[i].mesNas, dados[i].anoNas);
+        printf("Nota - %.1f.\n\n", dados[i].nota);
+    }
+}
+
 
 int main() {
-    int numeroAlunos;
+    int numeroAlunos, numeroAprovados = 0;
     setlocale(LC_ALL, "Portuguese");
 
     printf("Digite o número de alunos: ");
     scanf(" %d", &numeroAlunos);
 
-    struct aluno *dados = (struct aluno*) malloc(sizeof(struct aluno) * numeroAlunos); //Aloca Memória
+    struct aluno *dados = (struct aluno*) malloc(sizeof(struct aluno) * numeroAlunos); //Aloca Memória para Todos os Alunos
 
     preencheVetor(dados, numeroAlunos);
     imprimeVetor(dados, numeroAlunos);
+
+    for (int i = 0; i < numeroAlunos; i++) {
+        if (dados[i].nota >= 6) {
+            numeroAprovados++;
+        }
+    }
+    
+    struct aluno *registro = (struct aluno*) malloc(sizeof(struct aluno) * numeroAprovados); //Aloca Memória para os Alunos Aprovados
+
+    preencheRegistro(dados, registro, numeroAlunos, numeroAprovados);
+    imprimeVetor(registro, numeroAprovados);
+    
     free(dados);
+    free(registro);
 
     system("pause");
 }
